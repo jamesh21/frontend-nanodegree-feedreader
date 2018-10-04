85cc34b7c1a47bafadb335f8b5e6731ca43f6c04
James Ho <v-jaho@microsoft.com>
2018-10-03 18:33:59 -0700

    Finished with feed name testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,10 +27,9 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /*
+        * Loops through the feed and test if the url is empty or undefined
+        */
          it('Each feed should have a url defined', function() {
              for (let feed of allFeeds) {
                  expect(feed.url).toBeDefined();
@@ -45,9 +44,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         // it('', function() {
-         //
-         // });
+         it('Each feed should have a name defined', function() {
+             for (let feed of allFeeds) {
+                 expect(feed.name).toBeDefined();
+                 expect(feed.name.length).not.toBe(0);
+             }
+         });
     });
 
 
